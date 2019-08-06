f2499edf96d617a8bde84398091447d974c646a1
Tony <tonydangelo123@gmail.com>
2019-08-06 17:58:49 +0200

    refactor in multiple class

--- src/main.cpp
+++ src/main.cpp
@@ -1,95 +1,22 @@
 #include <Arduino.h>
 
-#include <WiFi.h>
-#include <WiFiClient.h>
-// https://pubsubclient.knolleary.net/api.html
-#include <PubSubClient.h>
-
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-#include <sleep.cpp>
-#include <mqtt.cpp>
+#include <sleep.h>
+#include <mqtt.h>
+#include <wifi.cpp>
 
 // Comment this out to disable prints and save space
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BME280 bme;
 
-float temperature, humidity, pressure, altitude;
-
-const char *ssid = "Batcave";
-const char *password = "iambrucewayne";
-
-const char *mqtt_client_id = "ESP32Garden";
-const char *mqtt_server = "farmer.cloudmqtt.com";
-const int mqtt_port = 18647;
-const char *mqtt_user = "jnwomnzt";
-const char *mqtt_password = "gOcaxgQSVTuA";
-
-// Lamp - LED - GPIO 4 = D2 on ESP-12E NodeMCU board
-const int lamp = 4;
-
-WiFiClient espClient;
-PubSubClient client(espClient);
+Mqtt mqtt;
 
 RTC_DATA_ATTR int bootCount = 0;
 
-void setup_wifi()
-{
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("WiFi connected - ESP IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-// This functions is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
-// Change the function below to add logic to your program, so when a device publishes a message to a topic that
-// your ESP8266 is subscribed you can actually do something
-void callback(String topic, byte *message, unsigned int length)
-{
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp;
-
-  for (int i = 0; i < length; i++)
-  {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
-  Serial.println();
-
-  // Here we can add more if statements to control more GPIOs with MQTT
-
-  // If a message is received on the topic room/lamp, you check if the message is either on or off. Turns the lamp GPIO according to the message
-  if (topic == "home/garden/light")
-  {
-    Serial.print("Changing garden/light to ");
-    if (messageTemp == "on")
-    {
-      digitalWrite(lamp, HIGH);
-      Serial.print("On");
-    }
-    else if (messageTemp == "off")
-    {
-      digitalWrite(lamp, LOW);
-      Serial.print("Off");
-    }
-  }
-  Serial.println();
-}
-
+// float temperature, humidity, pressure, altitude;
 
 void publish_values()
 {
@@ -113,8 +40,8 @@ void publish_values()
   dtostrf(h, 6, 2, humidityTemp);
 
   // Publishes Temperature and Humidity values
-  client.publish("home/garden/temperature", temperatureTemp);
-  client.publish("home/garden/humidity", humidityTemp);
+  mqtt.publish("home/garden/temperature", temperatureTemp);
+  mqtt.publish("home/garden/humidity", humidityTemp);
 
   // Log values to serial monitor
   Serial.print("Humidity: ");
@@ -128,37 +55,23 @@ void setup()
 {
   Serial.begin(9600);
   bme.begin(0x76);
+
   // Take some time to open up the Serial Monitor
   delay(1000);
 
-  // Increment boot number and print it every reboot
-  ++bootCount;
-  Serial.println("Boot number: " + String(bootCount));
-
   // Print the wakeup reason for ESP32
-  sleep::print_wakeup_reason();
+  Sleep::print_wakeup_reason(bootCount);
 
-  setup_wifi();
+  wifi::connect();
 
-  client.setServer(mqtt_server, mqtt_port);
+  mqtt.setup();
 
-  client.setCallback(callback);
-
-  if (!client.connected())
-  {
-    Serial.println("MQTT Client reconnect...");
-    reconnect();
-  }
-  if (!client.loop())
-  {
-    Serial.println("MQTT Client not in loop...");
-    client.connect(mqtt_client_id, mqtt_user, mqtt_password);
-  }
+  mqtt.connect();
 
   publish_values();
 
   // Go to sleep for 30 seconds
-  sleep::go_to_sleep();
+  Sleep::go_to_sleep();
 }
 
 void loop() {}
